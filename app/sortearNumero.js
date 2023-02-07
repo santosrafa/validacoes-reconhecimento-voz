639d19a5c541d1bc9f99ec70e2de90708a76a751
Your Name <ti.rafaelteles@gmail.com>
2023-02-07 10:31:50 -0300

    menor-valor

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,7 +1,16 @@
+const menorValor = 1
+const maiorValor = 100
+
 const numeroSecreto = geraNumeroAleatorio()
 
 function geraNumeroAleatorio() {
-    return parseInt(Math.random() * 100)
+    return parseInt(Math.random() * maiorValor + 1)
 }
 
 console.log('Número Secreto:',numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
