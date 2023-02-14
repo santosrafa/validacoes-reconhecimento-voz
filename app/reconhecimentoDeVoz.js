b428dc22344b0f54a850bc4b7f397024b64733d8
Your Name <ti.rafaelteles@gmail.com>
2023-02-13 22:01:54 -0300

    exibindo-mensagem

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -3,3 +3,9 @@ window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e){
+    console.log(e.results[0][0].transcript)
+}
