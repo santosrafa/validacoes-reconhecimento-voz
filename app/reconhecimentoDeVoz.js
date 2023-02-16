ec19e08e6bcdede9a02ce0b54417c744a0be0f19
Your Name <ti.rafaelteles@gmail.com>
2023-02-15 22:48:29 -0300

    chute

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.getElementById('chute')
+
 window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
@@ -7,5 +9,14 @@ recognition.start()
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e){
-    console.log(e.results[0][0].transcript)
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+        <div> Voce disse </div>
+        <span class="box">${chute}</span>
+    
+    `
 }
